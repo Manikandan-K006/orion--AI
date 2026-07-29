9c20b91fa8ec4823e09d47992b9caee014072896
FIZAL MOHAMED <fizalmohamed9730@mountzion.ac.in>
2026-07-29 14:00:31 +0530

    Next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   turbopack: {
-    root: __dirname,
-  }
+    root: path.resolve(__dirname),
+  },
 };
 
 export default nextConfig;
